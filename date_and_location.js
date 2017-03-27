c1a0d6b8b99826e6e22644b01d31d488252630f2
paulhennings <paul.hennings@gmail.com>
2017-03-26 21:13:10 -0700

    make date and place easier to update

---
+++ date_and_location.js
@@ -0,0 +1,7 @@
+document.getElementById("next_date").innerHTML = "TBD";
+document.getElementById("start_time").innerHTML = "TBD";
+document.getElementById("end_time").innerHTML = "TBD";
+document.getElementById("address_1").innerHTML = "Margaret T Hance Park";
+document.getElementById("address_2").innerHTML = "1202 North 3rd Street";
+document.getElementById("address_3").innerHTML = "Phoenix, AZ 85004";
+document.getElementById("directions").innerHTML = "Meet at the EAST end of Hance Park near Phoenix Center for the Arts."
