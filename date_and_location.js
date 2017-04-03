014d2a9c9c4e42119818de53cc57c331020eaa57
paulhennings <paul.hennings@gmail.com>
2017-04-03 08:02:16 -0700

    update for April 29

--- date_and_location.js
+++ date_and_location.js
@@ -1,6 +1,6 @@
 document.getElementById("next_date").innerHTML = "Saturday, April 29, 2017";
 document.getElementById("start_time").innerHTML = "Start Time: 10:00 AM";
-document.getElementById("end_time").innerHTML = "Ends at: NOON";
+document.getElementById("end_time").innerHTML = "Ending at: NOON";
 document.getElementById("address_1").innerHTML = "Margaret T Hance Park";
 document.getElementById("address_2").innerHTML = "1202 North 3rd Street";
 document.getElementById("address_3").innerHTML = "Phoenix, AZ 85004";
