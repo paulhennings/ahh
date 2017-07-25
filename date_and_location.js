815238a2d3ee72cd5fe72525010ac5ab6a0fa212
paulhennings <paulhennings@users.noreply.github.com>
2017-07-25 11:41:06 -0700

    Update date_and_location.js

--- date_and_location.js
+++ date_and_location.js
@@ -1,6 +1,6 @@
-document.getElementById("next_date").innerHTML = "Saturday, June 17, 2017";
-document.getElementById("start_time").innerHTML = "Start Time: 9:00 AM";
-document.getElementById("end_time").innerHTML = "Ending at: 11:00 AM";
+document.getElementById("next_date").innerHTML = "Saturday, July 29, 2017";
+document.getElementById("start_time").innerHTML = "Start Time: 8:00 AM";
+document.getElementById("end_time").innerHTML = "Ending at: 10:00 AM";
 document.getElementById("address_1").innerHTML = "Margaret T Hance Park";
 document.getElementById("address_2").innerHTML = "129 East Moreland Street";
 document.getElementById("address_3").innerHTML = "Phoenix, Arizona";
